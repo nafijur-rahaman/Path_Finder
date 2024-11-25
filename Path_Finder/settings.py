96729811e7a0d9f056bbded70aaa4d94166256d3
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2024-11-26 02:00:00 +0600

    add whitenoise

--- Path_Finder/settings.py
+++ Path_Finder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app","http://127.0.0.1:5501"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
